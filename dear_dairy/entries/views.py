97551523d431eb2035add41677c33fad0935202b
SoumyaRanjanNaik <srnaik2209@gmail.com>
2020-04-06 20:07:56 +0530

    handling forms and messages the manual way

--- dear_dairy/entries/views.py
+++ dear_dairy/entries/views.py
@@ -1,27 +1,49 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Entries
 from django.contrib import messages
 from .forms import EntryForm
+
 # Create your views here.
 def index(request):
     entries = Entries.objects.all()
-    context = {'entries': entries,'title':Entries.title,'text':Entries.text,'name':Entries.name,'date':Entries.date}
+    context = {
+        "entries": entries,
+        "title": Entries.title,
+        "text": Entries.text,
+        "name": Entries.name,
+        "date": Entries.date,
+    }
 
-    return render(request,'entries/index.html',context)
+    return render(request, "entries/index.html", context)
+
+
+# def add(request):
+#     if request.method == 'POST':
+#         form = EntryForm(request.POST)
+
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request,'Your Quote Is Successfully Posted!!')
+
+#            # return redirect('home')
+#     else:
+#         form = EntryForm()
+
+#     context = {'form' : form}
+
+#     return render(request, 'entries/add.html', context)
 
 
 def add(request):
-    if request.method == 'POST':
-        form = EntryForm(request.POST)
+    context = {}
+    if request.method == "POST":
+        title_data = request.POST.get("title")
+        name_data = request.POST.get("name")
+        quote_data = request.POST.get("quote")
 
-        if form.is_valid():
-            form.save()
-            messages.success(request,'Your Quote Is Successfully Posted!!')
-
-           # return redirect('home')
+        entry_object = Entries(title=title_data, text=quote_data, name=name_data)
+        entry_object.save()
+        context["msg"] = "entry has been saved"
+        return render(request, "entries/add.html", context)
     else:
-        form = EntryForm()
-
-    context = {'form' : form}
-
-    return render(request, 'entries/add.html', context)
+        return render(request, "entries/add.html")
